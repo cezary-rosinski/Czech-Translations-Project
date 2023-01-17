170a2321fc33ac3ce50a0f6866b6485b65a72b05
Cezary <cezary.rosinski@gmail.com>
2023-01-17 10:21:44 +0100

    function for manual corrections finished

--- translations_manual_corrections.py
+++ translations_manual_corrections.py
@@ -2,11 +2,10 @@ import pandas as pd
 import math
 from ast import literal_eval
 
-
+#%%
 
 data_df = pd.read_excel('translations_clean_database.xlsx')
-test = data_df.copy()
-ttt = pd.read_excel(r"C:\Users\Cezary\Downloads\manual deduplication.xlsx", sheet_name='reassignment')
+file_path = r"C:\Users\Cezary\Downloads\manual deduplication.xlsx"
 
 #deletion
 def deletion_phase(df, file_path):
@@ -43,59 +42,39 @@ def reassignment_phase(df, file_path):
     print('Reassignment phase completed')
     return df
 
-new_data_df = deletion_phase(data_df, r"C:\Users\Cezary\Downloads\manual deduplication.xlsx")
-new_data_df = reassignment_phase(new_data_df, r"C:\Users\Cezary\Downloads\manual deduplication.xlsx")
-   
-data_df.loc[data_df['001'] == 1175975131]['author_id']
-new_data_df.loc[new_data_df['001'] == 1175975131]['author_id']
-
-data_df.loc[data_df['001'] == 65993830]['language']
-new_data_df.loc[new_data_df['001'] == 65993830]['language']
-
-data_df.loc[data_df['001'] == 1175975131]['fiction_type']
-new_data_df.loc[new_data_df['001'] == 1175975131]['fiction_type']
-
-data_df.loc[data_df['001'] == 1175975131]['audience']
-new_data_df.loc[new_data_df['001'] == 1175975131]['audience']
-
-
 #deduplication
-print('Deduplication phase')
-deduplication_df = pd.read_excel(r"C:\Users\Cezary\Downloads\manual deduplication.xlsx", sheet_name='deduplication')
-deduplication_df['records ids'] = deduplication_df['records ids'].apply(lambda x: literal_eval(x))
-deduplication_df = deduplication_df.explode('records ids')
-
-deduplication_grouped = deduplication_df.groupby('proper record')
-
-
-for name, group in deduplication_grouped:
-    name = 237197114
-    group = deduplication_grouped.get_group(name)
-    group_ids = set(group['records ids'].to_list())
-    for _ in group_ids:
-        if int(_) not in data_df['001'].to_list():
-            print(f"---> Record {_} is not in the dataframe")
+def deduplication_phase(df, file_path):
+    print('Deduplication phase started')
+    deduplication_df = pd.read_excel(file_path, sheet_name='deduplication')
+    deduplication_df['records ids'] = deduplication_df['records ids'].apply(lambda x: literal_eval(x))
+    deduplication = list(zip(deduplication_df['records ids'], deduplication_df['proper record']))
+    for duplicates, proper in deduplication:
+        duplicates, proper = deduplication[0]
+        for _ in duplicates:
+            if int(_) not in [int(e) for e in df['001'].to_list()]:
+                print(f"---> Record {_} is not in the dataframe")
+        improper = [e for e in duplicates if e != proper]
+        changes = {'fiction_type', 'audience'}
+        
+        for column in changes:
+            extra_type = '❦'.join(df.loc[df['001'].isin(improper)][column].to_list())
+            final_type = '❦'.join(df.loc[df['001'] == proper][column].to_list())
+            final_type = final_type + '❦' + extra_type
+            df.loc[df['001'] == proper, column] = final_type
             
-            
-    for column in group:
-        if column in ['fiction_type', 'audience', '490', '500', '650', '655', '700']:
-            sub_group[column] = '❦'.join(sub_group[column].dropna().drop_duplicates().astype(str))
-        else:
-            try:
-                sub_group[column] = max(sub_group[column].dropna().astype(str), key=len)
-            except ValueError:
-                sub_group[column] = np.nan
-    df_oclc_deduplicated = df_oclc_deduplicated.append(sub_group)
-    
-    
-[e for e in data_df['001'].to_list() if e == 1187030940]
+        df = df.loc[~df['001'].isin(improper)]
+    print('Deduplication phase completed')    
+    return df
 
-data_df.loc[data_df['001'] == 4792338]['author_id']
-new_data_df.loc[new_data_df['001'] == 4792338]['author_id']
+def manual_corrections(df, file_path):
+    new_df = deletion_phase(df, file_path)
+    new_df = reassignment_phase(new_df, file_path)
+    new_df = deduplication_phase(new_df, file_path)
+    return new_df
 
+#%%main
 
-data_df.columns.values
-
+new_df = manual_corrections(data_df, file_path)
 
 
 
